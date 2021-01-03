007ace2cf50441984b3ef6fbd463348ba26aac1a
Kacper Wojak <kacperwojak17@gmail.com>
2021-01-03 10:25:29 +0100

    add searchbar, finish screen

--- app/src/main/java/com/example/imagine/screens/adapters/PhotosRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/imagine/screens/adapters/PhotosRecyclerViewAdapter.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
 import com.bumptech.glide.load.engine.GlideException
@@ -13,7 +12,6 @@ import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.target.Target
 import com.example.imagine.R
 import com.example.imagine.screens.adapters.view_holders.PhotosViewHolder
-import com.mikhaellopez.circularprogressbar.CircularProgressBar
 
 class PhotosRecyclerViewAdapter(private val listOfPhotos:ArrayList<String>):RecyclerView.Adapter<PhotosViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotosViewHolder {
