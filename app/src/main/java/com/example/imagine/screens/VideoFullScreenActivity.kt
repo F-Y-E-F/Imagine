f37ac1d22596b16bc1c9da2684793688f542b64b
Kacper Wojak <kacperwojak17@gmail.com>
2021-02-02 10:55:32 +0100

    add to favourites

--- app/src/main/java/com/example/imagine/screens/VideoFullScreenActivity.kt
+++ app/src/main/java/com/example/imagine/screens/VideoFullScreenActivity.kt
@@ -1,11 +1,16 @@
 package com.example.imagine.screens
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.method.LinkMovementMethod
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
+import com.example.imagine.ImagineApplication
 import com.example.imagine.R
+import com.example.imagine.favourite_database.video_database.FavouriteVideosViewModel
+import com.example.imagine.favourite_database.video_database.FavouriteVideosViewModelFactory
 import com.example.imagine.helpers.Texts
+import com.example.imagine.mvvm.models.photos.Photo
 import com.example.imagine.mvvm.models.videos.Video
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Renderer
@@ -14,16 +19,23 @@ import com.google.android.exoplayer2.ui.AspectRatioFrameLayout
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_video_full_screen.*
 
+
 class VideoFullScreenActivity : AppCompatActivity() {
+
+    private val favouriteVideosViewModel: FavouriteVideosViewModel by viewModels {
+        FavouriteVideosViewModelFactory((application as ImagineApplication).videosRepository)
+    }
+
     private lateinit var video: Video
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_video_full_screen)
 
-        video = Gson().fromJson(intent.getStringExtra("video"),Video::class.java)
+        video = Gson().fromJson(intent.getStringExtra("video"), Video::class.java)
 
         setupVideo()
         setupVideoInfo()
+        addToFavourite()
     }
 
     //---------------| Setup video in player view |-----------------
@@ -55,12 +67,44 @@ class VideoFullScreenActivity : AppCompatActivity() {
         videoComments.text = video.comments.toString()
         videoViews.text = video.views.toString()
 
-        videoOfficialSite.text = Texts.setupOfficialSiteLinkText(video.pageURL,this)
+        videoOfficialSite.text = Texts.setupOfficialSiteLinkText(video.pageURL, this)
         videoOfficialSite.movementMethod = LinkMovementMethod.getInstance()
 
         videoResolution.text = "Size : ${video.videos.medium.width}x${video.videos.medium.height}"
 
-        videoTags.text = Texts.setupTagsText(video.tags,this)
+        videoTags.text = Texts.setupTagsText(video.tags, this)
+
+    }
+
+    private fun addToFavourite(){
+
+        favouriteVideosViewModel.allVideos.observe(this){
+            var videoInDatabase : Video? = null
+            for(videoInDb in it){
+                if(videoInDb.id == video.id){
+                    videoInDatabase = videoInDb
+                    break
+                }
+            }
+
+            if(videoInDatabase==null){
+                addToFavVideosButton.text ="Add to favourites"
+                addToFavVideosButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_favorite_border, 0, 0, 0)
+            }else{
+                addToFavVideosButton.text ="Remove from favourites"
+                addToFavVideosButton.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_favourite, 0, 0, 0)
+            }
+
+            addToFavVideosButton.setOnClickListener {
+                if(videoInDatabase==null){
+                    favouriteVideosViewModel.insertFavVideo(video)
+                }else{
+                    favouriteVideosViewModel.deleteFavVideo(videoInDatabase!!)
+                }
+
+            }
+        }
+
 
     }
 }
