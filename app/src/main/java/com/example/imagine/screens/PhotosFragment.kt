bc37fcbd106c44d976823bac21dc3f0d13bddcd5
Kacper Wojak <kacperwojak17@gmail.com>
2021-02-07 17:57:23 +0100

    add settings simple fragment

--- app/src/main/java/com/example/imagine/screens/PhotosFragment.kt
+++ app/src/main/java/com/example/imagine/screens/PhotosFragment.kt
@@ -14,6 +14,7 @@ import android.view.inputmethod.EditorInfo
 import android.widget.*
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.example.imagine.R
@@ -63,6 +64,12 @@ class PhotosFragment : Fragment(), PhotosInterface {
             if (filters.visibility == View.GONE) filters.visibility = View.VISIBLE
             else filters.visibility = View.GONE
         }
+
+        settingsButton.setOnClickListener {
+            findNavController().navigate(R.id.action_photosFragment_to_settingsFragment)
+        }
+
+
     }
 
     //---------------| Get Start Photos |------------------
