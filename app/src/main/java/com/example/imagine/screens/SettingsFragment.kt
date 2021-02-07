caa46cff5d79178c6a3e7e8bc7c2645fd7beec3b
Kacper Wojak <kacperwojak17@gmail.com>
2021-02-07 19:08:33 +0100

    create simple settings fragment layout

--- app/src/main/java/com/example/imagine/screens/SettingsFragment.kt
+++ app/src/main/java/com/example/imagine/screens/SettingsFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.imagine.R
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 class SettingsFragment : Fragment() {
 
@@ -19,6 +20,11 @@ class SettingsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
 
+        arrayListOf<View>(settingsBackButton, settingsText).forEach {
+            it.setOnClickListener {
+                requireActivity().onBackPressed()
+            }
+        }
     }
 
 
